434870940348a5ada09a807046a13a790c306853
Ryan Ali <Ryanali456@gmail.com>
2022-12-17 15:10:38 +0800

    Create initial UI

--- server/controllers/v1/author.controller.go
+++ server/controllers/v1/author.controller.go
@@ -7,11 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Author struct {
+	GivenName  string `json:"givenName"`
+	MiddleName string `json:"middleName"`
+	Surname    string `json:"surname"`
+}
 type AuthorController struct{}
 
 func (ctrler *AuthorController) NewAuthor(ctx *gin.Context) {
 	ctx.JSON(definitions.Success(http.StatusOK, gin.H{}, "Author added."))
 }
 func (ctrler *AuthorController) GetAuthors(ctx *gin.Context) {
-	ctx.JSON(definitions.Success(http.StatusOK, gin.H{"authors": []gin.H{}}, "Authors fetched."))
+
+	var testAuthors []Author = []Author{{GivenName: "Ryan", MiddleName: "A.", Surname: "Ali"}, {GivenName: "Mark Allen", MiddleName: "A.", Surname: "Geamoga"}}
+	ctx.JSON(definitions.Success(http.StatusOK, gin.H{"authors": testAuthors}, "Authors fetched."))
 }
--- server/main.go
+++ server/main.go
@@ -7,15 +7,29 @@ import (
 
 	"slim-app/server/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		panic(envErr.Error())
+	}
+
+	SPA_ADMIN := os.Getenv("SPA_ADMIN_URL")
 	r := gin.New()
 	r.Use(gin.Recovery())
-
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{SPA_ADMIN},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	r.GET("/", func(ctx *gin.Context) {
 
 		ctx.JSON(http.StatusOK, gin.H{
