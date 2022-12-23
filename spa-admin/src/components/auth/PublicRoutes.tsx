f6ce270d1f5432c52def8da21af7b558a219eb33
Ryan Ali <Ryanali456@gmail.com>
2022-12-23 18:53:02 +0800

    create category schema

--- spa-admin/src/components/auth/PublicRoutes.tsx
+++ spa-admin/src/components/auth/PublicRoutes.tsx
@@ -1,12 +1,11 @@
-import { useContext} from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
-import { PublicRouteProps } from '../../definitions/props.definition';
-const PublicRoutes = ({restricted}:PublicRouteProps) => {
-    const { authenticated } = useContext(AuthContext);
-    if (authenticated && restricted) return <Navigate to={"/dashboard"} />;
-    return <Outlet/>
- };
-
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { PublicRouteProps } from "../../definitions/props.definition";
+const PublicRoutes = ({ restricted }: PublicRouteProps) => {
+  const { authenticated } = useContext(AuthContext);
+  if (authenticated && restricted) return <Navigate to={"/dashboard"} />;
+  return <Outlet />;
+};
 
 export default PublicRoutes;
