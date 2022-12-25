b23832d1a417aa8f800b4b31ff8656dc97141e86
Ryan Ali <Ryanali456@gmail.com>
2022-12-25 20:20:14 +0800

    fix merge conflict

--- spa-admin/src/pages/protected/books/schema.ts
+++ spa-admin/src/pages/protected/books/schema.ts
@@ -7,11 +7,7 @@ export const CreateAuthorSchema = object().shape({
     
 })
 export const CategorySchema = object().shape({
-<<<<<<< Updated upstream
-    name: string().test("is-alpha","Category name should not contain numbers, symbols and whitespaces",  (value) => validator.isAlpha(value ?? "")).required("Name is required field")
-=======
     name: string().test("is-alpha","Name should not contain symbols and whitespaces",  (value) => validator.isAlphanumeric(value ?? "")).required("Name is required field")
->>>>>>> Stashed changes
 })
 // 
 export const PublisherSchema = object().shape({
