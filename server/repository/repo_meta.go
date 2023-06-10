73f5a1057a50360006ef3bfe9d4c4652658bff2c
Ryan Ali <Ryanali456@gmail.com>
2023-06-10 18:52:48 +0800

    wip: add return to previous page implementation if active page is empty on delete author

--- server/repository/repo_meta.go
+++ server/repository/repo_meta.go
@@ -6,9 +6,9 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func GetRepositoryMetadataTx(transaction * sqlx.Tx,tableName string , rowsLimit int)(Metadata, error){
+func GetParanoidRepositoryMetadataTx(transaction * sqlx.Tx,tableName string , rowsLimit int)(Metadata, error){
 		meta := Metadata{}
-        query := fmt.Sprintf("SELECT CASE WHEN COUNT(*) = 0 then 0 else CEIL((COUNT(*)/$1::numeric))::bigint end as pages, count(*) as records FROM %s", tableName )
+        query := fmt.Sprintf("SELECT CASE WHEN COUNT(*) = 0 then 0 else CEIL((COUNT(*)/$1::numeric))::bigint end as pages, count(*) as records FROM %s where deleted_at is null", tableName )
 		getMetaErr := transaction.Get(&meta, query, rowsLimit)
 		return meta, getMetaErr
 }
