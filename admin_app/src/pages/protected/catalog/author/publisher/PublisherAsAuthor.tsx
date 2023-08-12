795ae46ca703fd6db265c176bf18c918c3abfb93
Ryan Ali <Ryanali456@gmail.com>
2023-08-12 16:21:55 +0800

    hide pagination if page count is less than 1 in author page

--- admin_app/src/pages/protected/catalog/author/publisher/PublisherAsAuthor.tsx
+++ admin_app/src/pages/protected/catalog/author/publisher/PublisherAsAuthor.tsx
@@ -59,6 +59,8 @@ const PublisherAsAuthor = () => {
     queryFn: fetchPublisher,
     queryKey: ["publishers", currentPage],
   });
+  const paginationClass =
+    totalPages <= 1 ? "hidden" : "flex gap-2 items-center";
   return (
     <>
       <div>
@@ -120,7 +122,7 @@ const PublisherAsAuthor = () => {
             onPageChange={({ selected }) => {
               setCurrentPage(selected + 1);
             }}
-            className="flex gap-2 items-center"
+            className={paginationClass}
             previousLabel="Previous"
             previousClassName="px-2 border text-gray-500 py-1 rounded"
             nextClassName="px-2 border text-blue-500 py-1 rounded"
