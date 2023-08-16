bf5107d5fca9df157a3d0ead10a7cc162d1f6128
Ryan Ali <Ryanali456@gmail.com>
2023-08-16 17:16:00 +0800

    display new permission in access control page

--- server/app/acl/permissions.go
+++ server/app/acl/permissions.go
@@ -1,7 +1,7 @@
 package acl
 
 
-type Module struct {
+type Permission struct {
 	Id int `json:"id"`
 	Name string `json:"name"`
 	Value string `json:"value"`
@@ -9,59 +9,70 @@ type Module struct {
 }
 
 
-var Modules = []Module{
+var Permissions = []Permission{
 	{
 		Id: 1,
+		Name: "Book Module",
 		Value:"Book.Access",
 		Description: "Allows user to access book module.",
 	},
 	{
 		Id: 2, 
+		Name: "Author Module",
 		Value: "Author.Access",
 		Description: "Allows user to access author module.",
 	},
 	{
 		Id: 3,
+		Name: "Publisher Module",
 		Value: "Publisher.Access",
 		Description: "Allows user to access publisher module.",
 	},
 	{
 		Id: 4,
+		Name: "Section Module",
 		Value: "Section.Access", 
 		Description: "Allows user to access book section module." ,
 	},
 	{
 		Id: 5,
+		Name: "Source of Fund Module",
 		Value:"FundSource.Access",
 		Description: "Allows user to access source of fund module.",
 	},
 	{
 		Id: 6,
+		Name: "Account Module",
 		Value:"Account.Access",
 		Description: "Allows user to access account module.",
 	},
 	{
 		Id: 7,
+		Name: "Inventory Module",
 		Value: "Audit.Access",
 		Description: "Allows user to access audit module.",
 	},
 	{
 		Id: 8,
+		Name: "Access Control Module",
 		Value: "ACL.Access",
 		Description: "Allows user to access access control module.",
 	},
 	{
 		Id: 9,
+		Name: "Settings Module",
 		Value: "Settings.Access",
 		Description: "Allows user to access settings module.",
 	},
 	{
 		Id: 10, 
+		Name: "Borrowing Module",
 		Value: "Borrowing.Access",
 		Description: "Allows user to access borrowing module.",
 	},
 	{
 		Id: 11, 
+		Name: "Penalty Module",
 		Value: "Penalty.Access",
 		Description: "Allows user to access penalty module.",
 	},
