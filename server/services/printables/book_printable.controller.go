cc53f70ff7cc99f63e57b3ee00c2d2bd38e384ce
Ryan Ali <Ryanali456@gmail.com>
2023-09-12 18:43:21 +0800

    render title card and author card

--- server/services/printables/book_printable.controller.go
+++ server/services/printables/book_printable.controller.go
@@ -1,6 +1,7 @@
 package printables
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/RyanAliXII/sti-munoz-library-system/server/app/http/httpresp"
@@ -19,9 +20,38 @@ func(p * Printable)RenderBookPrintables(ctx * gin.Context) {
 		 ctx.JSON(httpresp.Fail404(nil, "Book not found."))
 		 return
 	}
-	ctx.HTML(http.StatusOK, "printables-generator/books/index", book)
+	authors := make([]string, 0)
+	for _, author := range book.Authors.People {
+		authors = append(authors, fmt.Sprintf("%s %s", author.GivenName, author.Surname))
+	}
+	for _, author := range book.Authors.Organizations {
+		authors = append(authors, author.Name)
+	}
+	for _, author := range book.Authors.Publishers {
+		authors = append(authors,author.Name)
+	}
+	ctx.HTML(http.StatusOK, "printables-generator/books/index", gin.H{
+		"book": book,
+		"authors": authors,
+		"formateddEdition": ordinal(book.Edition),
+	})
 }
+func ordinal(n int) string {
+    if n >= 11 && n <= 13 {
+        return fmt.Sprintf("%dth", n)
+    }
 
+    switch n % 10 {
+    case 1:
+        return fmt.Sprintf("%dst", n)
+    case 2:
+        return fmt.Sprintf("%dnd", n)
+    case 3:
+        return fmt.Sprintf("%drd", n)
+    default:
+        return fmt.Sprintf("%dth", n)
+    }
+}
 type PrintableController interface {
 	RenderBookPrintables(c * gin.Context)
 }
