3211ce6690f196116bfbe1e0da22b7e52447a5bb
Ryan Ali <Ryanali456@gmail.com>
2023-09-18 16:29:09 +0800

    remove book circulatio services and model

---
+++ server/model/bag.model.go
@@ -0,0 +1,13 @@
+package model
+
+
+type BagItem struct {
+	Id string `json:"id" db:"id"`
+	AccessionId  string `json:"accessionId" db:"accession_id"`
+	AccountId string `json:"accountId" db:"account_id"`
+	AccessionNumber int `json:"accessionNumber" db:"number"`
+	CopyNumber int `json:"copyNumber" db:"copy_number"`
+	Book BookJSON `json:"book" db:"book"`
+	IsAvailable bool `json:"isAvailable" db:"is_available"`
+	IsChecked bool `json:"isChecked" db:"is_checked"`
+}
