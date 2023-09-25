356352505386fc1c36cf0bb098a3644302756f7b
Ryan Ali <Ryanali456@gmail.com>
2023-09-26 01:03:48 +0800

    fetch client logs from the backend

--- admin_app/src/pages/protected/system/client-log/ClientLogPage.tsx
+++ admin_app/src/pages/protected/system/client-log/ClientLogPage.tsx
@@ -6,8 +6,25 @@ import {
   Th,
   Thead,
 } from "@components/ui/table/Table";
+import { useRequest } from "@hooks/useRequest";
+import { useQuery } from "@tanstack/react-query";
 
 const ClientLogPage = () => {
+  const { Get } = useRequest();
+  const fetchClientLogs = async () => {
+    try {
+      const { data: response } = await Get("/client-logs/");
+      console.log(response?.data?.clientLogs);
+      return response?.data?.clientLogs ?? [];
+    } catch (error) {
+      return [];
+    }
+  };
+
+  const { data: clientLogs } = useQuery({
+    queryFn: fetchClientLogs,
+    queryKey: ["clientLogs"],
+  });
   return (
     <div className="w-full lg:w-11/12 bg-white p-6 lg:p-10 -md lg:rounded-md mx-auto mb-10">
       <div className="w-full flex justify-between mb-5"></div>
