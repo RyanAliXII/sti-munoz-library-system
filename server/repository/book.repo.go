1fc2aca6d9fd5dc19ffbf3b0e3028cd8c7abcf51
Ryan Ali <Ryanali456@gmail.com>
2023-09-27 19:04:32 +0800

    implement bulk import

--- server/repository/book.repo.go
+++ server/repository/book.repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"mime/multipart"
@@ -490,11 +491,16 @@ func (repo * BookRepository)AddBookCopies(id string, copies int) error{
 	transaction.Commit()
 	return nil
 }
-func (repo * BookRepository)ImportBooks(books []model.BookImport) error{
+func (repo * BookRepository)ImportBooks(books []model.BookImport, sectionId int) error{
 	var bookTitleId string;
+	var lastBookTitleId string;
+	var lastBookId string
 	var maxAccessionNumber int;
-	var author model.Author
-	var publisher model.Publisher
+	var copyNumber = 1
+
+	var authorStore map[string]string = make(map[string]string)
+	var publisherStore map[string]string = map[string]string{}
+	dialect := goqu.Dialect("postgres")
 	if (len(books) == 0){
 			return nil
 	}
@@ -503,36 +509,158 @@ func (repo * BookRepository)ImportBooks(books []model.BookImport) error{
 		transaction.Rollback()
 		return err
 	}
+	authorRows := make([]goqu.Record, 0)
+	publisherRows := make([]goqu.Record, 0)
+	bookRows := make([]goqu.Record, 0)
+	bookAuthorRows := make([]goqu.Record, 0)
+	accessionRows := make([]goqu.Record, 0)
 	for _, book := range books {
 		if(book.AccessionNumber > maxAccessionNumber ){
 				maxAccessionNumber = book.AccessionNumber
 		}
+		authorNameTrimmed := strings.TrimSpace(book.Author)
+		publisherNameTrimmed := strings.TrimSpace(book.Publisher)
 		bookTitleId = strings.Map(func(r rune) rune {
 			if(unicode.IsSpace(r)){
 					return -1
 				}
 				return unicode.To(unicode.LowerCase, r)
 		}, book.Title)
-		if bookTitleId == "" {
-				book.Title = "Untitled"
+		if authorNameTrimmed == "" {
+			authorNameTrimmed= "Unknown"
 		}
-		err := transaction.Get(&author, "INSERT INTO catalog.author(name) VALUES($1) returning id", book.Author)
-		if err != nil {
-            transaction.Rollback()
-			return err
-		}	
-		err = transaction.Get(&publisher, "INSERT INTO catalog.publisher(name) VALUES ($1) returning id", book.Publisher)
-		if err != nil {
+		if publisherNameTrimmed == "" {
+			publisherNameTrimmed = "Unknown"
+		}
+		if book.Title ==  ""{
+			book.Title = "Untitled"
+		}
+		
+
+		authorId, isAuthorStored := authorStore[authorNameTrimmed];
+		publisherId, isPublisherStored := publisherStore[publisherNameTrimmed]
+		if !isAuthorStored {
+			err := transaction.Get(&authorId, "SELECT id from catalog.author where name = $1 LIMIT 1", authorNameTrimmed)
+			if err != nil  && err != sql.ErrNoRows {
+				transaction.Rollback()
+				return err
+			}
+			if len(authorId) == 0 {
+				id, err  := uuid.NewUUID()
+				if err != nil {
+					transaction.Rollback()
+					return err
+				}
+				authorId = id.String()
+				authorRows = append(authorRows, goqu.Record{
+					"id":  authorId,
+					"name": authorNameTrimmed,
+				})
+				
+			}
+			authorStore[authorNameTrimmed] = authorId
+		}
+		if !isPublisherStored {
+			err = transaction.Get(&publisherId, "SELECT id from catalog.publisher where name = $1 LIMIT 1", publisherNameTrimmed)
+			if err != nil && err != sql.ErrNoRows {
+				transaction.Rollback()
+				return err
+			}
+			if len(publisherId) == 0 {
+				id, err  := uuid.NewUUID()
+				if err != nil {
+					transaction.Rollback()
+					return err
+				}
+				publisherId = id.String()
+				publisherRows = append(publisherRows, goqu.Record{
+					"id":  publisherId,
+					"name": publisherNameTrimmed,
+				})
+			}
+			publisherStore[publisherNameTrimmed] = publisherId
+		}
+		if(bookTitleId != lastBookTitleId){
+			id, err := uuid.NewUUID()
+			if err != nil {
+				transaction.Rollback()
+				return err
+			}
+			lastBookId = id.String()
+			copyNumber = 1
+			bookRows = append(bookRows, goqu.Record{
+				"id": lastBookId,
+				"title": book.Title,
+				"publisher_id" : publisherId,
+				"section_id" :sectionId,
+			})
+			bookAuthorRows = append(bookAuthorRows, goqu.Record{
+				"book_id":  lastBookId,
+				"author_id": authorId,
+			})
+		}
+		accessionRows = append(accessionRows, goqu.Record{
+				"book_id": lastBookId,
+				"number" : book.AccessionNumber,
+				"copy_number" : copyNumber,
+				"section_id" :sectionId,
+		})
+		copyNumber ++;
+		lastBookTitleId = bookTitleId
+	 }
+	
+	if(len(accessionRows) > 0){
+		authorDs := dialect.From(goqu.T("author").Schema("catalog")).Prepared(true).Insert().Rows(authorRows)
+		query, args, _ := authorDs.ToSQL()
+		_, err = transaction.Exec(query, args...)
+		if err  != nil {	
+			fmt.Println("IN INSERT AUTHOR")
 			transaction.Rollback()
 			return err
-			}
 		}
-	
-   transaction.Rollback()
-   fmt.Println(bookTitleId)
-   fmt.Println(author)
-   fmt.Println(maxAccessionNumber)
-   fmt.Println(publisher)
+	}
+	if(len(publisherRows) > 0){
+		publisherDs := dialect.From(goqu.T("publisher").Schema("catalog")).Prepared(true).Insert().Rows(publisherRows)
+		query, args, _ := publisherDs.ToSQL()
+		_, err = transaction.Exec(query, args...)
+		if err  != nil {	
+			fmt.Println("IN INSERT PUBLISHER")
+			transaction.Rollback()
+			return err
+		}
+	}
+   if(len(bookRows) > 0){
+	bookDs := dialect.From(goqu.T("book").Schema("catalog")).Prepared(true).Insert().Rows(bookRows)
+	query, args, _ := bookDs.ToSQL()
+	_, err = transaction.Exec(query, args...)
+	if err  != nil {	
+		fmt.Println("IN INSERT BOOK")
+		transaction.Rollback()
+		return err
+	}
+   }
+	if(len(bookAuthorRows) > 0){
+		bookAuthorDs := dialect.From(goqu.T("book_author").Schema("catalog")).Prepared(true).Insert().Rows(bookAuthorRows)
+		query, args, _ := bookAuthorDs.ToSQL()
+		_, err = transaction.Exec(query, args...)
+		if err  != nil {	
+			fmt.Println("IN INSERT BOOKAUTHOR")
+			transaction.Rollback()
+			return err
+		}
+	}
+	if(len(accessionRows) > 0){
+		accessionDs := dialect.From(goqu.T("accession").Schema("catalog")).Prepared(true).Insert().Rows(accessionRows)
+		query, args, _ := accessionDs.ToSQL()
+		_, err = transaction.Exec(query, args...)
+		if err  != nil {	
+			fmt.Println("IN INSERT ACCESSION")
+			transaction.Rollback()
+			return err
+		}
+	}
+
+   	transaction.Commit()
 	return nil
 }
 func NewBookRepository() BookRepositoryInterface {
@@ -553,6 +681,6 @@ type BookRepositoryInterface interface {
 	UpdateBookCover(bookId string, covers []*multipart.FileHeader) error
 	AddBookCopies(id string, copies int) error
 	DeleteBookCoversByBookId(bookId string) error 
-	ImportBooks(books []model.BookImport) error
+	ImportBooks(books []model.BookImport, sectionId int) error
 
 }
