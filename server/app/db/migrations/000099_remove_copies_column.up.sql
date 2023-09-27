94875d40305ad165729aaa1894bf83afcd875841
Ryan Ali <Ryanali456@gmail.com>
2023-09-27 20:51:48 +0800

    remove book copies column and change title data type

---
+++ server/app/db/migrations/000099_remove_copies_column.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE IF EXISTS catalog.book
+DROP COLUMN IF EXISTS copies;
---
+++ server/app/db/migrations/000101_recreate_search_vector_table.up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE catalog.book
+ADD COLUMN IF NOT EXISTS search_vector tsvector
+generated always as (
+    setweight(to_tsvector('english', title), 'A')
+    || ' ' ||
+    setweight(to_tsvector('english', description), 'B')::tsvector
+) stored;
