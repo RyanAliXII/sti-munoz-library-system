f59771db00b8f8dff83ac0bc9a3f5acc51e7da64
Ryan Ali <Ryanali456@gmail.com>
2023-09-29 04:08:56 +0800

    fix add author modal in book add form

--- admin_app/src/pages/protected/catalog/book-add/author-selection/AuthorSelectionModal.tsx
+++ admin_app/src/pages/protected/catalog/book-add/author-selection/AuthorSelectionModal.tsx
@@ -2,8 +2,10 @@ import { ModalProps } from "@definitions/types";
 
 import Modal from "react-responsive-modal";
 import AuthorSelection from "./AuthorSelection";
+import { useBookAddFormContext } from "../BookAddFormContext";
 
 const AuthorSelectionModal = ({ closeModal, isOpen }: ModalProps) => {
+  const { form } = useBookAddFormContext();
   return (
     <>
       <Modal
@@ -24,11 +26,10 @@ const AuthorSelectionModal = ({ closeModal, isOpen }: ModalProps) => {
           <div className="mb-3">
             <h3 className="text-2xl"> Authors</h3>
             <small>
-              You have selected:
+              You have selected:{" "}
               <span className="font-semibold">
-                {/* {" "}
-                {numberOfAuthorSelected}{" "}
-                {numberOfAuthorSelected > 1 ? "Authors" : "Author"} */}
+                {form.authors.length}{" "}
+                {form.authors.length > 1 ? "Authors" : "Author"}
               </span>
             </small>
 
