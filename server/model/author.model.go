aa650baef9353ba5f744479eb9d3846e13302ecf
Ryan Ali <Ryanali456@gmail.com>
2023-09-29 01:14:55 +0800

    display books and temporarily limit the books to 50

--- server/model/author.model.go
+++ server/model/author.model.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
 
 type Author struct {
 	Id    string    `json:"id" db:"id"`
@@ -8,26 +13,22 @@ type Author struct {
 
 type AuthorsJSON []Author
 
-// func (instance *AuthorsJSON) Scan(value interface{}) error {
-// 	val, valid := value.([]byte)
-// 	INITIAL_DATA_ON_ERROR := AuthorsJSON{
-// 		People:        make(PeopleAsAuthorJSON, 0),
-// 		Organizations: make(OrgsAsAuthorJSON, 0),
-// 		Publishers:    make(PublishersJSON, 0),
-// 	}
-// 	if valid {
-// 		unmarshalErr := json.Unmarshal(val, instance)
-// 		if unmarshalErr != nil {
-// 			*instance = INITIAL_DATA_ON_ERROR
-// 		}
-// 	} else {
-// 		*instance = INITIAL_DATA_ON_ERROR
-// 	}
-// 	return nil
-// }
-// func (copy AuthorsJSON) Value(value interface{}) (driver.Value, error) {
-// 	return copy, nil
-// }
+func (instance *AuthorsJSON) Scan(value interface{}) error {
+	val, valid := value.([]byte)
+	INITIAL_DATA_ON_ERROR := AuthorsJSON{}
+	if valid {
+		unmarshalErr := json.Unmarshal(val, instance)
+		if unmarshalErr != nil {
+			*instance = INITIAL_DATA_ON_ERROR
+		}
+	} else {
+		*instance = INITIAL_DATA_ON_ERROR
+	}
+	return nil
+}
+func (copy AuthorsJSON) Value(value interface{}) (driver.Value, error) {
+	return copy, nil
+}
 
 // type PeopleAsAuthorJSON []struct {
 // 	PersonAsAuthor
