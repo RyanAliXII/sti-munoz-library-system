83ee469ac40eeda3d4778568b3dc1a627d8f949f
Ryan Ali <Ryanali456@gmail.com>
2023-10-05 17:09:38 +0800

    separate file of book printables component

--- admin_app/src/pages/protected/catalog/book-page/BookPage.tsx
+++ admin_app/src/pages/protected/catalog/book-page/BookPage.tsx
@@ -41,6 +41,7 @@ import ReactPaginate from "react-paginate";
 import usePaginate from "@hooks/usePaginate";
 import { isValid } from "date-fns";
 import useDebounce from "@hooks/useDebounce";
+import { BookPrintablesModal } from "./BookPrintablesModal";
 
 const BookPage = () => {
   const {
@@ -70,7 +71,7 @@ const BookPage = () => {
   const { currentPage, totalPages, setTotalPages, setCurrentPage } =
     usePaginate({
       initialPage: initCurrentPage,
-      numberOfPages: 0,
+      numberOfPages: 1,
     });
   const [searchKeyword, setSearchKeyword] = useState<string>("");
   useEffect(() => {
@@ -274,68 +275,5 @@ const BookPage = () => {
     </>
   );
 };
-interface PrintablesModalProps extends ModalProps {
-  book: Book;
-}
 
-export const BookPrintablesModal: React.FC<PrintablesModalProps> = ({
-  closeModal,
-  isOpen,
-  book,
-}) => {
-  const printableDiv = useRef<HTMLDivElement | null>(null);
-
-  const fetchPDF = async () => {
-    const response = await axios.get(
-      `http://localhost:5200/printables/books/${book.id}`,
-      {
-        responseType: "blob",
-      }
-    );
-    const url = URL.createObjectURL(response.data);
-    return url;
-  };
-  const {
-    data: pdfURL,
-    isFetching,
-    isError,
-  } = useQuery<string>({
-    queryFn: fetchPDF,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    initialData: "",
-    queryKey: ["bookPrintable", book],
-  });
-
-  if (!isOpen) return null;
-  return (
-    <Modal
-      showCloseIcon={false}
-      open={isOpen}
-      onClose={closeModal}
-      styles={{
-        modal: {
-          maxWidth: "none",
-        },
-      }}
-      classNames={{
-        modal: "w-11/12  lg:w-12/12  xl: rounded h-[900-px]",
-      }}
-    >
-      <div className="py-2 mx-auto" ref={printableDiv}>
-        <LoadingBoundaryV2 isLoading={isFetching} isError={isError}>
-          <div className="mb-5">
-            <LighButton onClick={closeModal}>Close</LighButton>
-          </div>
-          <iframe
-            className="w-full h-[900-px]"
-            src={pdfURL}
-            height="900px"
-          ></iframe>
-        </LoadingBoundaryV2>
-        <div className="flex flex-col w-full gap-2 mb-2 "></div>
-      </div>
-    </Modal>
-  );
-};
 export default BookPage;
