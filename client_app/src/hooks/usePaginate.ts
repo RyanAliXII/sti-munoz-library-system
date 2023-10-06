8dcfe04c894246ec5de3954e5908135de70b5653
Ryan Ali <Ryanali456@gmail.com>
2023-10-06 20:20:03 +0800

    add pagination, search and loading in client app book catalog

---
+++ client_app/src/hooks/usePaginate.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const usePaginate = ({
+  initialPage = 1,
+  numberOfPages = 0,
+  useURLParamsAsState = true,
+}: {
+  initialPage: number | (() => number);
+  numberOfPages: number;
+  useURLParamsAsState?: boolean;
+}) => {
+  const [currentPage, setCurrentPage] = useState<number>(initialPage);
+  const [totalPages, setTotalPages] = useState<number>(numberOfPages);
+  const [params, setUrlSearchParams] = useSearchParams();
+  const nextPage = () => {
+    if (currentPage === totalPages) return;
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+  const previousPage = () => {
+    if (currentPage === 1) return;
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  if (useURLParamsAsState) {
+    useEffect(() => {
+      setUrlSearchParams({ page: currentPage.toString() });
+    }, [currentPage]);
+    useEffect(() => {
+      const page = params.get("page");
+      if (page) {
+        setCurrentPage(parseInt(page));
+      }
+    }, [params]);
+  }
+
+  return {
+    currentPage,
+    totalPages,
+    nextPage,
+    previousPage,
+    setTotalPages,
+    setCurrentPage,
+  };
+};
+
+export default usePaginate;
