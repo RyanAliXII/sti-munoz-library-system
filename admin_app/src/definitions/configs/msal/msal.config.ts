038a651c3d2bbc6afc06f3e7b0b4fba7566c03aa
Ryan Ali <Ryanali456@gmail.com>
2023-10-07 00:01:51 +0800

    use .envs in client app and admin app

--- admin_app/src/definitions/configs/msal/msal.config.ts
+++ admin_app/src/definitions/configs/msal/msal.config.ts
@@ -1,16 +1,13 @@
 import { PopupRequest, Configuration } from "@azure/msal-browser";
 
-export const CLIENT_ID = "1b3617d9-7634-43f9-acf2-bd45c0b45ad6";
-export const API_CLIENT_ID = "770a2b1b-6004-4a51-94a8-cd008797ba92";
-
-const uri = import.meta.env.PROD
-  ? "https://stimunozlibrary.online:5201"
-  : "http://localhost:5201";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const API_CLIENT_ID = import.meta.env.VITE_API_CLIENT_ID;
+export const uri = import.meta.env.VITE_REDIRECT_URI;
+export const TENANT_ID = import.meta.env.VITE_TENANT_ID;
 const msalConfig: Configuration = {
   auth: {
     clientId: CLIENT_ID,
-    authority:
-      "https://login.microsoftonline.com/87731d3d-9f08-4782-b34e-5979bb65be87",
+    authority: `https://login.microsoftonline.com/${TENANT_ID}`,
     redirectUri: uri,
     postLogoutRedirectUri: uri,
   },
--- client_app/src/definitions/api.config.ts
+++ client_app/src/definitions/api.config.ts
@@ -1,4 +1,2 @@
-const HOST = import.meta.env.PROD
-  ? "https://stimunozlibrary.online:5200"
-  : "http://localhost:5200";
+const HOST = import.meta.env.VITE_SERVER_URL;
 export const BASE_URL_V1 = `${HOST}/api/1`;
