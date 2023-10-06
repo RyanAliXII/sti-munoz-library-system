fb6f6844afffc6ed78bd99bf216663852d1bab05
Ryan Ali <Ryanali456@gmail.com>
2023-10-06 13:35:56 +0800

    fix pagination not backing on browser back

--- admin_app/src/hooks/usePaginate.ts
+++ admin_app/src/hooks/usePaginate.ts
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const usePaginate = ({
   initialPage = 1,
@@ -9,7 +10,7 @@ const usePaginate = ({
 }) => {
   const [currentPage, setCurrentPage] = useState<number>(initialPage);
   const [totalPages, setTotalPages] = useState<number>(numberOfPages);
-
+  const [params, setUrlSearchParams] = useSearchParams();
   const nextPage = () => {
     if (currentPage === totalPages) return;
     setCurrentPage((prevPage) => prevPage + 1);
@@ -18,6 +19,16 @@ const usePaginate = ({
     if (currentPage === 1) return;
     setCurrentPage((prevPage) => prevPage - 1);
   };
+
+  useEffect(() => {
+    setUrlSearchParams({ page: currentPage.toString() });
+  }, [currentPage]);
+  useEffect(() => {
+    const page = params.get("page");
+    if (page) {
+      setCurrentPage(parseInt(page));
+    }
+  }, [params]);
   return {
     currentPage,
     totalPages,
