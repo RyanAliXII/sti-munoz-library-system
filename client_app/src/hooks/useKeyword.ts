43cbe31401c53614b5aacfa508bd2a705fa00301
Ryan Ali <Ryanali456@gmail.com>
2023-10-08 17:16:46 +0800

    create use keyword hooks

---
+++ client_app/src/hooks/useKeyword.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const useKeyword = ({
+  initialKeyword = "",
+  useURLParamsAsState = true,
+}: {
+  initialKeyword: string | (() => string);
+  useURLParamsAsState?: boolean;
+}) => {
+  const [keyword, setKeyword] = useState<string>(initialKeyword);
+
+  const [params, setUrlSearchParams] = useSearchParams();
+  if (useURLParamsAsState) {
+    useEffect(() => {
+      setUrlSearchParams({ keyword: keyword });
+    }, [keyword]);
+    useEffect(() => {
+      const paramKeyword = params.get("keyword");
+      if (paramKeyword) setKeyword(paramKeyword);
+    }, [params]);
+  }
+
+  return {
+    keyword,
+  };
+};
+
+export default useKeyword;
