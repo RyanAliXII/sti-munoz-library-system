2b69a4994e3471cd170348954bc7c12f9ab507bb
Ryan Ali <Ryanali456@gmail.com>
2023-11-09 19:01:01 +0800

    fix ui in inventory page

--- admin_app/src/components/BookSearchBox.tsx
+++ admin_app/src/components/BookSearchBox.tsx
@@ -13,7 +13,7 @@ type BookSearchBoxProps = {
   label?: string;
 };
 
-const HIGHLIGHTED_CLASS = "bg-gray-100";
+const HIGHLIGHTED_CLASS = "bg-gray-100 dark:bg-gray-600";
 const BookSearchBox = ({ selectBook, label }: BookSearchBoxProps) => {
   const [searchKeyword, setKeyword] = useState("");
   const debounce = useDebounce();
@@ -64,7 +64,8 @@ const BookSearchBox = ({ selectBook, label }: BookSearchBoxProps) => {
               {...getInputProps({
                 placeholder: "Enter book's title or description",
                 onChange: handleSearchBoxChange,
-                className: InputClasses.InputDefaultClasslist,
+                className:
+                  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
               })}
             />
 
@@ -73,7 +74,7 @@ const BookSearchBox = ({ selectBook, label }: BookSearchBoxProps) => {
                 <ul
                   {...getMenuProps({
                     className:
-                      "w-full absolute list-none max-h-80 bg-white overflow-y-auto cursor-pointer border mt-2 rounded z-10",
+                      "w-full absolute list-none max-h-80 bg-white overflow-y-auto cursor-pointer border mt-2 rounded z-10 dark:bg-gray-700 dark:border-gray-600 small-scroll",
                   })}
                 >
                   {books?.map((book, index) => {
@@ -83,13 +84,15 @@ const BookSearchBox = ({ selectBook, label }: BookSearchBoxProps) => {
                         {...getItemProps({
                           key: book.id,
                           item: book,
-                          className: `p-3 border flex flex-col ${
+                          className: `p-3 border flex dark:border-gray-600 flex-col ${
                             index === highlightedIndex ? HIGHLIGHTED_CLASS : ""
                           }`,
                         })}
                       >
-                        <span className="text-gray-600">{book.title}</span>
-                        <small className="text-gray-400">
+                        <span className="text-gray-600 dark:text-gray-100">
+                          {book.title}
+                        </span>
+                        <small className="text-gray-300">
                           ISBN: {book.isbn} | Authors: {authors.join(", ")}
                         </small>
                       </li>
@@ -97,14 +100,14 @@ const BookSearchBox = ({ selectBook, label }: BookSearchBoxProps) => {
                   })}
                 </ul>
               ) : (
-                <div className="w-full absolute list-none h-52 overflow-y-auto cursor-pointer items-center flex justify-center bg-white border rounded mt-2">
+                <div className="w-full absolute list-none h-52 overflow-y-auto cursor-pointer items-center flex justify-center bg-white border rounded mt-2 dark:bg-gray-700">
                   <ClipLoader color="#C5C5C5" />
                 </div>
               )
             ) : null}
 
             {isOpen && books.length === 0 ? (
-              <div className="w-full absolute list-none h-52 bg-white overflow-y-auto cursor-pointer border mt-2 rounded flex items-center justify-center">
+              <div className="w-full absolute list-none h-52 bg-white overflow-y-auto cursor-pointer border mt-2 rounded flex items-center justify-center dark:bg-gray-700 dark:border-none">
                 <span className="text-gray-400 text-sm">
                   No result found for {searchKeyword}
                 </span>
