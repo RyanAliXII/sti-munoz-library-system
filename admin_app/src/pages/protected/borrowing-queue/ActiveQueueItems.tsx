089ef3d35bf3d55ca10f046d3ac65eefbcda381d
Ryan Ali <Ryanali456@gmail.com>
2023-11-19 15:36:45 +0800

    breakdown queue items component

--- admin_app/src/pages/protected/borrowing-queue/ActiveQueueItems.tsx
+++ admin_app/src/pages/protected/borrowing-queue/ActiveQueueItems.tsx
@@ -1,14 +1,12 @@
 import Container from "@components/ui/container/Container";
-import TableContainer from "@components/ui/table/TableContainer";
-import { useQueueItems } from "@hooks/data-fetching/borrowing-queue";
-import { Button, Table } from "flowbite-react";
-import ordinal from "ordinal";
-import { useParams } from "react-router-dom";
-import { FaArrowDown, FaArrowUp, FaSave } from "react-icons/fa";
-import Tippy from "@tippyjs/react";
-import { useReducer } from "react";
-import { queueItemReducer } from "./queue-item-reducer";
 import { BorrowingQueueItem } from "@definitions/types";
+import { useQueueItems } from "@hooks/data-fetching/borrowing-queue";
+import { Button } from "flowbite-react";
+import { useReducer } from "react";
+import { FaSave } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import { queueItemReducer } from "./queue-item-reducer";
+import ActiveQueueItemsTable from "./ActiveQueueItemsTable";
 
 const ActiveQueueItems = () => {
   const [items, dispatch] = useReducer(queueItemReducer, []);
@@ -56,70 +54,11 @@ const ActiveQueueItems = () => {
           </div>
         </Button>
       </div>
-      <TableContainer>
-        <Table>
-          <Table.Head>
-            <Table.HeadCell>Book</Table.HeadCell>
-            <Table.HeadCell>Client</Table.HeadCell>
-            <Table.HeadCell>Position</Table.HeadCell>
-            <Table.HeadCell></Table.HeadCell>
-          </Table.Head>
-          <Table.Body>
-            {items.map((item, idx) => {
-              const account = item.client;
-              const name =
-                account.givenName.length + account.surname.length === 0
-                  ? "Unnamed"
-                  : `${account.givenName} ${account.surname}`;
-              return (
-                <Table.Row key={item.id}>
-                  <Table.Cell>
-                    <div className="font-semibold text-gray-900 dark:text-white">
-                      {item.book.title}
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <div className="text-base font-semibold text-gray-900 dark:text-white">
-                      {name}
-                    </div>
-                    <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                      {account.email}
-                    </div>
-                  </Table.Cell>
-                  <Table.Cell>{ordinal(idx + 1)}</Table.Cell>
-                  <Table.Cell>
-                    <div className="flex gap-2">
-                      <Tippy content="Move Up">
-                        <Button
-                          color="primary"
-                          outline
-                          onClick={() => {
-                            moveUp(idx, item);
-                          }}
-                        >
-                          <FaArrowUp />
-                        </Button>
-                      </Tippy>
-                      <Tippy content="Move Down">
-                        <Button
-                          color="primary"
-                          outline
-                          onClick={() => {
-                            moveDown(idx, item);
-                          }}
-                        >
-                          <FaArrowDown />
-                        </Button>
-                      </Tippy>
-                      <Button color="failure">Remove</Button>
-                    </div>
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-      </TableContainer>
+      <ActiveQueueItemsTable
+        items={items}
+        moveDown={moveDown}
+        moveUp={moveUp}
+      />
     </Container>
   );
 };
---
+++ admin_app/src/pages/protected/borrowing-queue/ActiveQueueItemsTable.tsx
@@ -0,0 +1,86 @@
+import TableContainer from "@components/ui/table/TableContainer";
+import { BorrowingQueueItem } from "@definitions/types";
+import Tippy from "@tippyjs/react";
+import { Button, Table } from "flowbite-react";
+import ordinal from "ordinal";
+import { FC } from "react";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+type QueueItemsTableProps = {
+  items: BorrowingQueueItem[];
+  moveUp: (currentPosition: number, item: BorrowingQueueItem) => void;
+  moveDown: (currentPosition: number, item: BorrowingQueueItem) => void;
+};
+const ActiveQueueItemsTable: FC<QueueItemsTableProps> = ({
+  items,
+  moveDown,
+  moveUp,
+}) => {
+  return (
+    <TableContainer>
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>Book</Table.HeadCell>
+          <Table.HeadCell>Client</Table.HeadCell>
+          <Table.HeadCell>Position</Table.HeadCell>
+          <Table.HeadCell></Table.HeadCell>
+        </Table.Head>
+        <Table.Body>
+          {items.map((item, idx) => {
+            const account = item.client;
+            const name =
+              account.givenName.length + account.surname.length === 0
+                ? "Unnamed"
+                : `${account.givenName} ${account.surname}`;
+            return (
+              <Table.Row key={item.id}>
+                <Table.Cell>
+                  <div className="font-semibold text-gray-900 dark:text-white">
+                    {item.book.title}
+                  </div>
+                </Table.Cell>
+                <Table.Cell>
+                  <div className="text-base font-semibold text-gray-900 dark:text-white">
+                    {name}
+                  </div>
+                  <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    {account.email}
+                  </div>
+                </Table.Cell>
+                <Table.Cell>{ordinal(idx + 1)}</Table.Cell>
+                <Table.Cell>
+                  <div className="flex gap-2">
+                    <Tippy content="Move Up">
+                      <Button
+                        color="primary"
+                        outline
+                        onClick={() => {
+                          moveUp(idx, item);
+                        }}
+                      >
+                        <FaArrowUp />
+                      </Button>
+                    </Tippy>
+                    <Tippy content="Move Down">
+                      <Button
+                        color="primary"
+                        outline
+                        onClick={() => {
+                          moveDown(idx, item);
+                        }}
+                      >
+                        <FaArrowDown />
+                      </Button>
+                    </Tippy>
+                    <Button color="failure">Remove</Button>
+                  </div>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default ActiveQueueItemsTable;
