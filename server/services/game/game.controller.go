82a7dd30c2fd4d033480199c1734e9b8cf92097e
Ryan Ali <Ryanali456@gmail.com>
2023-11-20 16:19:00 +0800

    create a model for game

--- server/services/game/game.controller.go
+++ server/services/game/game.controller.go
@@ -2,7 +2,10 @@ package game
 
 import (
 	"github.com/RyanAliXII/sti-munoz-library-system/server/app/http/httpresp"
+	"github.com/RyanAliXII/sti-munoz-library-system/server/app/pkg/slimlog"
+	"github.com/RyanAliXII/sti-munoz-library-system/server/model"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 type Game struct {}
@@ -15,6 +18,13 @@ func NewGameController () GameController{
 }
 
 func (ctrler * Game)NewGame (ctx * gin.Context) {
+	game := model.Game{}
+	err := ctx.ShouldBindBodyWith(&game, binding.JSON)
+	if err != nil {
+		logger.Error(err.Error(), slimlog.Error("bindErr"))
+		ctx.JSON(httpresp.Fail400(nil, "Unknown error occured."))
+		return
+	}
 
 	
 	ctx.JSON(httpresp.Success200(nil, "Game added."))
