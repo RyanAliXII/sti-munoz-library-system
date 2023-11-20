dcc97065b8318f9eefcb1344c32d19beef3a7b75
Ryan Ali <Ryanali456@gmail.com>
2023-11-20 16:33:04 +0800

    add createdAt and deleted_at column

--- server/app/db/migrations/000130_create_games_table.up.sql
+++ server/app/db/migrations/000130_create_games_table.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS services.game (
    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
    name VARCHAR(150) DEFAULT '',
-   description TEXT DEFAULT ''
+   description TEXT DEFAULT '',
+   deleted_at timestamptz,
+	created_at timestamptz DEFAULT NOW()
 )
