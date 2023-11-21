89b2bc9cd6ee4728694d1fc1bfa372d45e94b4d7
Ryan Ali <Ryanali456@gmail.com>
2023-11-22 00:09:17 +0800

    create a timeslot routes

---
+++ server/services/timeslot/timeslot.route.go
@@ -0,0 +1,12 @@
+package timeslot
+
+import (
+	"github.com/RyanAliXII/sti-munoz-library-system/server/app/http/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+
+func TimeSlotRoutes(router * gin.RouterGroup){
+	ctrler := NewTimeSlotProfileController()
+	router.POST("/profile", middlewares.BlockRequestFromClientApp, ctrler.NewProfile)
+}
