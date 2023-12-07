949ff6255519788ae8bca0bf021902ea8e9df360
Ryan Ali <Ryanali456@gmail.com>
2023-12-07 18:22:17 +0800

    refactor book page

--- admin_app/src/hooks/data-fetching/book.ts
+++ admin_app/src/hooks/data-fetching/book.ts
@@ -1,5 +1,12 @@
+import { Book, Metadata } from "@definitions/types";
 import { useRequest } from "@hooks/useRequest";
-import { MutationOptions, useMutation } from "@tanstack/react-query";
+import {
+  MutationOptions,
+  QueryFunction,
+  UseQueryOptions,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
 
 type MigrateForm = { sectionId: number; bookIds: string[] };
 export const useMigrateCollection = ({
@@ -18,5 +25,59 @@ export const useMigrateCollection = ({
     onSuccess: onSuccess,
     onError: onError,
     onSettled: onSettled,
+  });
+};
+
+type BookFilter = {
+  page: number;
+  keyword: string;
+};
+type UseBookData = {
+  books: Book[];
+  metadata: Metadata;
+};
+export const useBooks = ({
+  queryKey,
+  onSuccess,
+}: UseQueryOptions<
+  UseBookData,
+  unknown,
+  UseBookData,
+  [string, BookFilter]
+>) => {
+  const { Get } = useRequest();
+  const fetchBooks: QueryFunction<UseBookData, [string, BookFilter]> = async ({
+    queryKey,
+  }) => {
+    const filter = queryKey[1];
+    try {
+      const { data: response } = await Get("/books/", {
+        params: {
+          page: filter?.page ?? 0,
+          keyword: filter?.keyword ?? "",
+        },
+      });
+      return {
+        books: response?.data?.books ?? [],
+        metadata: {
+          pages: 0,
+          records: 0,
+        },
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        books: [],
+        metadata: {
+          pages: 0,
+          records: 0,
+        },
+      };
+    }
+  };
+  return useQuery<UseBookData, unknown, UseBookData, [string, BookFilter]>({
+    queryFn: fetchBooks,
+    queryKey: queryKey,
+    onSuccess: onSuccess,
   });
 };
--- admin_app/src/pages/protected/catalog/book-page/BookPage.tsx
+++ admin_app/src/pages/protected/catalog/book-page/BookPage.tsx
@@ -1,28 +1,26 @@
+import HasAccess from "@components/auth/HasAccess";
+import Container from "@components/ui/container/Container";
+import { DangerConfirmDialog } from "@components/ui/dialog/Dialog";
 import { Book, Section } from "@definitions/types";
+import { useBooks, useMigrateCollection } from "@hooks/data-fetching/book";
+import useDebounce from "@hooks/useDebounce";
+import { useRequest } from "@hooks/useRequest";
 import { useSwitch } from "@hooks/useToggle";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, TextInput } from "flowbite-react";
 import { ChangeEvent, useMemo, useReducer, useState } from "react";
-import { AiOutlineEdit, AiOutlinePlus, AiOutlinePrinter } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import Container from "@components/ui/container/Container";
-import HasAccess from "@components/auth/HasAccess";
-import { useRequest } from "@hooks/useRequest";
-import { LoadingBoundaryV2 } from "@components/loader/LoadingBoundary";
-import Tippy from "@tippyjs/react";
+import { AiOutlinePlus } from "react-icons/ai";
 import { TbDatabaseImport } from "react-icons/tb";
-import ImportBooksModal from "./ImportBooksModal";
-import useDebounce from "@hooks/useDebounce";
-import { isValid } from "date-fns";
-import { Button, Checkbox, Dropdown, Table, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import { useSearchParamsState } from "react-use-search-params-state";
 import { BookPrintablesModal } from "./BookPrintablesModal";
-import CustomPagination from "@components/pagination/CustomPagination";
-import { bookSelectionReducer } from "./bookselection-reducer";
-import { array } from "yup";
+import ImportBooksModal from "./ImportBooksModal";
 import MigrateModal from "./MigrateModal";
-import { DangerConfirmDialog } from "@components/ui/dialog/Dialog";
-import { useMigrateCollection } from "@hooks/data-fetching/book";
-import { toast } from "react-toastify";
+import { bookSelectionReducer } from "./bookselection-reducer";
+import BookTable from "./BookTable";
+import { LoadingBoundaryV2 } from "@components/loader/LoadingBoundary";
+import CustomPagination from "@components/pagination/CustomPagination";
 
 const BookPage = () => {
   const {
@@ -36,25 +34,7 @@ const BookPage = () => {
     page: { type: "number", default: 1 },
     keyword: { type: "string", default: "" },
   });
-  const { Get } = useRequest();
-  const fetchBooks = async () => {
-    try {
-      const { data: response } = await Get("/books/", {
-        params: {
-          page: filterParams?.page,
-          keyword: filterParams?.keyword,
-        },
-      });
-      if (response?.data?.metadata) {
-        const page = response?.data?.metadata?.pages;
-        setTotalPages(page ?? 0);
-      }
-      return response?.data?.books ?? [];
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  };
+
   const searchDebounce = useDebounce();
 
   const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
@@ -75,14 +55,6 @@ const BookPage = () => {
 
     openPrintablesModal();
   };
-  const {
-    data: books,
-    isError,
-    isFetching,
-  } = useQuery<Book[]>({
-    queryFn: fetchBooks,
-    queryKey: ["books", filterParams],
-  });
 
   const {
     close: closeImportModal,
@@ -93,7 +65,17 @@ const BookPage = () => {
   const [bookSelections, dispatchSelection] = useReducer(bookSelectionReducer, {
     books: new Map<string, Book>(),
   });
-
+  const {
+    data: bookData,
+    isFetching,
+    isError,
+  } = useBooks({
+    onSuccess: () => {},
+    queryKey: [
+      "books",
+      { keyword: filterParams?.keyword, page: filterParams?.page },
+    ],
+  });
   const onSelect = (event: ChangeEvent<HTMLInputElement>, book: Book) => {
     const isChecked = event.target.checked;
     if (isChecked) {
@@ -206,94 +188,25 @@ const BookPage = () => {
           </div>
         </div>
         <LoadingBoundaryV2 isLoading={isFetching} isError={isError}>
-          <div className="overflow-x-auto">
-            <div className="inline-block min-w-full align-middle">
-              <Table className="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
-                <Table.Head>
-                  <Table.HeadCell></Table.HeadCell>
-                  <Table.HeadCell>Title</Table.HeadCell>
-                  <Table.HeadCell>Authors</Table.HeadCell>
-                  <Table.HeadCell>Copies</Table.HeadCell>
-                  <Table.HeadCell>Year Published</Table.HeadCell>
-                  <Table.HeadCell></Table.HeadCell>
-                </Table.Head>
-                <Table.Body className="divide-y dark:divide-gray-700">
-                  {books?.map((book) => {
-                    const author = book.authors?.[0]?.name ?? "";
-                    return (
-                      <Table.Row key={book.id}>
-                        <Table.Cell>
-                          <Checkbox
-                            color="primary"
-                            checked={bookSelections.books.has(book?.id ?? "")}
-                            onChange={(event) => {
-                              onSelect(event, book);
-                            }}
-                          />
-                        </Table.Cell>
-
-                        <Table.Cell>
-                          <div className="text-base font-semibold text-gray-900 dark:text-white">
-                            {book.title}
-                          </div>
-                          <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                            {book.section.name}
-                          </div>
-                        </Table.Cell>
-                        <Table.Cell>{author}</Table.Cell>
-                        <Table.Cell>{book.copies}</Table.Cell>
-                        <Table.Cell>{book.yearPublished}</Table.Cell>
-
-                        <Table.Cell className="flex gap-3">
-                          <Tippy content="View Printables">
-                            <Button
-                              color="primary"
-                              onClick={() => {
-                                setBookForPrintingAndOpenModal(book);
-                              }}
-                            >
-                              <AiOutlinePrinter className="text-lg cursor-pointer " />
-                            </Button>
-                          </Tippy>
-                          <HasAccess
-                            requiredPermissions={[
-                              "Book.Access",
-                              "Publisher.Access",
-                              "Section.Access",
-                              "Author.Access",
-                            ]}
-                          >
-                            <Tippy content="Edit Book">
-                              <Button
-                                as={Link}
-                                color="secondary"
-                                to={`/books/edit/${book.id}`}
-                              >
-                                <AiOutlineEdit className="text-lg cursor-pointer" />
-                              </Button>
-                            </Tippy>
-                          </HasAccess>
-                        </Table.Cell>
-                      </Table.Row>
-                    );
-                  })}
-                </Table.Body>
-              </Table>
-              <div className="p-4">
-                <CustomPagination
-                  nextLabel="Next"
-                  pageRangeDisplayed={5}
-                  pageCount={totalPages}
-                  forcePage={filterParams?.page - 1}
-                  onPageChange={({ selected }) => {
-                    setFilterParams({ page: selected + 1 });
-                  }}
-                  isHidden={totalPages <= 1}
-                  previousLabel="Previous"
-                  renderOnZeroPageCount={null}
-                />
-              </div>
-            </div>
+          <BookTable
+            books={bookData?.books ?? []}
+            bookSelections={bookSelections}
+            onSelect={onSelect}
+            setBookForPrintingAndOpenModal={setBookForPrintingAndOpenModal}
+          />
+          <div className="p-4">
+            <CustomPagination
+              nextLabel="Next"
+              pageRangeDisplayed={5}
+              pageCount={totalPages}
+              forcePage={filterParams?.page - 1}
+              onPageChange={({ selected }) => {
+                setFilterParams({ page: selected + 1 });
+              }}
+              isHidden={totalPages <= 1}
+              previousLabel="Previous"
+              renderOnZeroPageCount={null}
+            />
           </div>
         </LoadingBoundaryV2>
       </Container>
