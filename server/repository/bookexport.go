d68797ee975ffe5e833134ffa16ae8ea411a3d52
Ryan Ali <Ryanali456@gmail.com>
2024-01-25 12:33:07 +0800

    create a separate data fetching function for csv

--- server/repository/bookexport.go
+++ server/repository/bookexport.go
@@ -30,19 +30,93 @@ var excelHeaderColumns = []string{
 	// "author",
 	// "ebook",
 }
+var alphabet = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+func toChar(i int) string {
+    return alphabet[i-1]
+}
 var excelSheet = "Books"
-// func(repo * BookRepository)GetBooksByCollectionId(collectionId string)([]model.Accession, error) {
-// 	accessions := make([]model.Accession, 0)
-//     query := `
-// 	SELECT accession.id, accession.number, copy_number, book.json_format as book,
-// 	accession.book_id
-// 	FROM catalog.accession
-// 	INNER JOIN book_view as book on accession.book_id = book.id 
-// 	where book.section_id = $1
-// 	`
-// 	err := repo.db.Select(&accessions, query, collectionId)
-// 	return accessions, err
-// }
+
+func(repo * BookRepository)ExportBooks(collectionId int, fileType string)(*bytes.Buffer, error) {
+	buffer := new(bytes.Buffer)
+	if(fileType == ".xlsx"){
+		file, err := repo.processExcel(collectionId)
+		if err != nil {
+			return buffer, err
+		}
+		 err = file.Write(buffer)
+		if err != nil {
+			return buffer, err
+		}
+		return buffer, nil
+	}
+	if(fileType == ".csv"){
+		csvBuffer, err := repo.processCSV(collectionId)
+		if err != nil {
+			return buffer, err
+		}
+		return csvBuffer, nil
+	}
+	return buffer, fmt.Errorf("unsupported file type: %s", fileType)
+}
+
+func(repo * BookRepository)processExcel(collectionId int) (*excelize.File, error) {
+	f := excelize.NewFile()
+	accessions, err := repo.getExcelDataByCollectionId(collectionId)
+	if err != nil {
+		return f, err
+	}
+	if(len(accessions) == 0){
+		return f, nil
+	}
+    defer func() {
+        if err := f.Close(); err != nil {
+			logger.Error(err.Error(), slimlog.Error("error while closing excel file"))
+        }
+    }()
+	sheetIdx, err := f.NewSheet(excelSheet)
+	if err != nil {
+		return f,err
+	}
+	err = repo.buildExcelHeaders(f)
+	if err != nil {
+		return f,err
+	}
+	
+	err = repo.addExcelData(2, accessions, f)
+	if err != nil {
+		return f,err
+	}
+	f.SetActiveSheet(sheetIdx)
+	
+	
+	return f, err
+}
+func(repo * BookRepository)addExcelData(startingRow int, accessions []map[string]interface{}, f * excelize.File) error {
+	for rowCursor, accession := range accessions {
+		for colCursor, col  := range excelHeaderColumns{
+			char := toChar(colCursor + 1)
+			cell := fmt.Sprintf("%s%d", char, rowCursor + startingRow)
+			err  := f.SetCellValue(excelSheet, cell, accession[col])
+			
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+func( repo *  BookRepository)buildExcelHeaders(f * excelize.File) error {
+	for idx, col := range excelHeaderColumns {
+		char := toChar(idx + 1)
+		cell := fmt.Sprintf(`%s1`, char)
+		err := f.SetCellValue(excelSheet, cell, col)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func(repo * BookRepository)getExcelDataByCollectionId(collectionId int)([]map[string]interface{}, error) {
 	results := []map[string]interface{}{}
@@ -108,31 +182,25 @@ func(repo * BookRepository)getExcelDataByCollectionId(collectionId int)([]map[st
 }
 
 
-func(repo * BookRepository)ExportBooks(collectionId int, fileType string)(*bytes.Buffer, error) {
-	buffer := new(bytes.Buffer)
-	if(fileType == ".xlsx"){
-		file, err := repo.processExcel(collectionId)
-		if err != nil {
-			return buffer, err
-		}
-		 err = file.Write(buffer)
-		if err != nil {
-			return buffer, err
-		}
-		return buffer, nil
-	}
-	if(fileType == ".csv"){
-		csvBuffer, err := repo.processCSV(collectionId)
-		if err != nil {
-			return buffer, err
-		}
-		return csvBuffer, nil
-	}
-	return buffer, fmt.Errorf("unsupported file type: %s", fileType)
-}
 func (repo * BookRepository)processCSV(collectionId int) (*bytes.Buffer, error) {
-	accessions := make([]model.BookExport, 0)
 	buffer := new(bytes.Buffer)
+	accessions, err := repo.getCSVDataByCollectionId(collectionId)
+	if err != nil {
+		return buffer,err
+	}
+	bytes, err := gocsv.MarshalBytes(&accessions)
+	if err != nil {
+		return buffer, err
+	}
+	_, err = buffer.Write(bytes)
+	if err != nil {
+		return buffer, err
+	}
+	return buffer, nil
+}
+
+func (repo * BookRepository)getCSVDataByCollectionId(collectionId int)([]model.BookExport, error) {
+	accessions := make([]model.BookExport, 0)
 	query := `
 	SELECT  accession.number as accession_number, copy_number, book.title, 
 	book.id as book_id,
@@ -153,80 +221,7 @@ func (repo * BookRepository)processCSV(collectionId int) (*bytes.Buffer, error) 
 	ORDER BY accession_number ASC
 	`
 	err := repo.db.Select(&accessions, query, collectionId)
-	if err != nil {
-		return buffer,err
-	}
-	bytes, err := gocsv.MarshalBytes(&accessions)
-	if err != nil {
-		return buffer, err
-	}
-	_, err = buffer.Write(bytes)
-	if err != nil {
-		return buffer, err
-	}
-	return buffer, nil
-}
-func(repo * BookRepository)processExcel(collectionId int) (*excelize.File, error) {
-	f := excelize.NewFile()
-	accessions, err := repo.getExcelDataByCollectionId(collectionId)
-	if err != nil {
-		return f, err
-	}
-	if(len(accessions) == 0){
-		return f, nil
-	}
-    defer func() {
-        if err := f.Close(); err != nil {
-			logger.Error(err.Error(), slimlog.Error("error while closing excel file"))
-        }
-    }()
-	sheetIdx, err := f.NewSheet(excelSheet)
-	if err != nil {
-		return f,err
-	}
-	err = repo.buildExcelHeaders(f)
-	if err != nil {
-		return f,err
-	}
-	
-	err = repo.addExcelData(2, accessions, f)
-	if err != nil {
-		return f,err
-	}
-	f.SetActiveSheet(sheetIdx)
-	
-	
-	return f, err
-}
-func(repo * BookRepository)addExcelData(startingRow int, accessions []map[string]interface{}, f * excelize.File) error {
-	for rowCursor, accession := range accessions {
-		for colCursor, col  := range excelHeaderColumns{
-			char := toChar(colCursor + 1)
-			cell := fmt.Sprintf("%s%d", char, rowCursor + startingRow)
-			err  := f.SetCellValue(excelSheet, cell, accession[col])
-			
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-func( repo *  BookRepository)buildExcelHeaders(f * excelize.File) error {
-	for idx, col := range excelHeaderColumns {
-		char := toChar(idx + 1)
-		cell := fmt.Sprintf(`%s1`, char)
-		err := f.SetCellValue(excelSheet, cell, col)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return accessions, err
+
 }
 
-
-var alphabet = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-func toChar(i int) string {
-    return alphabet[i-1]
-}
