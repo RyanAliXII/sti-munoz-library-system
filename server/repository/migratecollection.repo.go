a7293e03c62b78a0fa9c8af22c50b8026d60427c
Ryan Ali <Ryanali456@gmail.com>
2024-02-15 09:31:34 +0800

    fix collection migration

--- server/repository/migratecollection.repo.go
+++ server/repository/migratecollection.repo.go
@@ -13,79 +13,49 @@ func (repo * BookRepository)MigrateCollection(sectionId int, bookIds []string)er
 	if len(bookIds) == 0 {
 		return nil
 	}
-	count := 0
     dialect := goqu.Dialect("postgres")
 	collection := model.Section{}
 	err := repo.db.Get(&collection, "SELECT id, accession_table from catalog.section where id = $1 LIMIT 1", sectionId)
 	if err != nil {
 		return err
 	}
-	//get first book in the list
-	book := repo.GetOne(bookIds[0])
-	ds := dialect.Select(goqu.COUNT(goqu.I("id"))).Prepared(true).From("book_view").Where(exp.Ex{"id": bookIds, "accession_table": book.AccessionTable})
+	ds := dialect.Select(goqu.C("accession_table"), goqu.C("id")).Prepared(true).From("book_view").Where(exp.Ex{"id": bookIds})
 	query, args, err := ds.ToSQL()
 	if err != nil {
 		return err
 	}
+	books := make([]model.Book, 0)
 
-	err = repo.db.Get(&count, query, args...)
+	err = repo.db.Select(&books, query, args...)
 	if err != nil {
 		return err
 	}
-	if count != len(bookIds) {
-		return fmt.Errorf("book section are not all the same")
-	}
-	updateDs := dialect.Update(goqu.T("book").Schema("catalog")).Prepared(true)
-    if(collection.AccessionTable != book.AccessionTable){
-		transaction, err := repo.db.Beginx()
-		if err != nil {
-			transaction.Rollback()
-			return err
-		}
-		updateDs = updateDs.Set(goqu.Record{
-			"section_id" : collection.Id,
-		}).Where(goqu.Ex{"id": bookIds})
-		query, args ,err = updateDs.ToSQL()
-		if err != nil {
-			transaction.Rollback()
-			return err
-		}
-		_, err = transaction.Exec(query, args...)
-	
-		if err != nil {
-			transaction.Rollback()
-			return err
-		}
-		
-		updateDs := dialect.Update(goqu.T("accession").Schema("catalog")).Set(goqu.Record{
-			"number": goqu.L(fmt.Sprintf("get_next_id('%s')", collection.AccessionTable)),
-		}).Where(goqu.Ex{"book_id": bookIds})
-
-		query, args ,err = updateDs.ToSQL()
-		if err != nil {
-			transaction.Rollback()
-			return err
-		}
-		_, err = transaction.Exec(query, args...)
-		
-		if err != nil {
-			transaction.Rollback()
-			return err
-		}
-		transaction.Commit()
-		return nil
-	}
-	updateDs = updateDs.Set(goqu.Record{
-		"section_id" : collection.Id,
-	}).Where(goqu.Ex{"id": bookIds})
-
-	query, args ,err = updateDs.ToSQL()
+	transaction, err := repo.db.Beginx()
 	if err != nil {
 		return err
 	}
-	_, err = repo.db.Exec(query, args...)
-	if err != nil {
-		return err
+	for _, book := range books{
+		_, err := transaction.Exec("UPDATE catalog.book SET section_id = $1 where id = $2", collection.Id, book.Id)
+		if err != nil {
+			transaction.Rollback()
+			return err
+		}
+		if(book.AccessionTable != collection.AccessionTable){
+			query := fmt.Sprintf("UPDATE catalog.accession SET number = get_next_id('%s'), section_id = $1  where book_id = $2", collection.AccessionTable)
+			_, err := transaction.Exec(query, collection.Id, book.Id)
+			if err != nil {
+				transaction.Rollback()
+				return err
+			}
+		}else{
+			query := "UPDATE catalog.accession SET section_id = $1 where book_id = $2"
+			_, err := transaction.Exec(query, collection.Id, book.Id)
+			if err != nil {
+				transaction.Rollback()
+				return err
+			}
+		}
 	}
+	transaction.Commit()
 	return nil
 }
