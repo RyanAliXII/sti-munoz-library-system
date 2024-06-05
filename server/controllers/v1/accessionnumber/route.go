e39b8a2293e7566e66022ee6104f7b2871b5ce91
Ryan Ali <Ryanali456@gmail.com>
2024-06-05 12:57:40 +0800

    register accession number routes

--- server/controllers/v1/accessionnumber/route.go
+++ server/controllers/v1/accessionnumber/route.go
@@ -1,1 +1,12 @@
 package accessionnumber
+
+import (
+	"github.com/RyanAliXII/sti-munoz-library-system/server/services"
+	"github.com/gin-gonic/gin"
+)
+
+
+func AccessionNumberRoutes(router * gin.RouterGroup, services * services.Services){
+	ctrler := NewAccessionNumberController(services)
+	router.GET("", ctrler.GetAccessionNumber)
+}
