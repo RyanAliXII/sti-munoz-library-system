7c54b26dcb18252ca66f51de3a6a08c7cbe2395e
Ryan Ali <ryanali456@gmail.com>
2025-03-03 16:48:26 +0800

    add locking in permission store

--- server/app/pkg/permissionstore/postgrestore.go
+++ server/app/pkg/permissionstore/postgrestore.go
@@ -2,61 +2,82 @@ package permissionstore
 
 import (
 	"fmt"
-	"slices"
+	"sync"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 )
-type PostgresPermissionCache struct {
-	Permissions []string
-	ValidUntil  time.Time
-}
-type PostgresPermissionStore struct {
-	db    *sqlx.DB
-	store map[string]PostgresPermissionCache
-}
-func NewPostgresPermissionStore(db *sqlx.DB) PermissionStore{
-	return &PostgresPermissionStore{
-		db: db,
-		store: make(map[string]PostgresPermissionCache),
-	}
-}
-func (s *PostgresPermissionStore) HasPermission(accountId string, permissions []string) bool {
-	cache, isInCache := s.store[accountId]
-	if !isInCache || time.Now().After(cache.ValidUntil) {
-		
-		cache = PostgresPermissionCache{
-			Permissions: []string{},
-			ValidUntil:  time.Now().Add(time.Minute * 15),
-		}
-		query := `
-			SELECT system.role_permission.value
-			FROM system.account_role
-			INNER JOIN account_view AS account ON account_role.account_id = account.id
-			INNER JOIN system.role ON account_role.role_id = role.id
-			INNER JOIN system.role_permission ON role.id = role_permission.role_id 
-			WHERE account.id = $1
-		`
-		err := s.db.Select(&cache.Permissions, query, accountId)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		s.store[accountId] = cache
-	}
-	for _, permission := range permissions {
-		hasPermission := slices.Contains(cache.Permissions, permission)
-		if hasPermission {
-			return true
-		}
-	}
 
-	return false
+type PostgresPermissionCache struct {
+    Permissions []string
+    ValidUntil  time.Time
 }
-func(s * PostgresPermissionStore)Invalidate(){
-	for key := range s.store {
-		s.store[key] = PostgresPermissionCache{
-			Permissions: []string{},
-			ValidUntil: time.Now(),
-		}
-	}
+
+type PostgresPermissionStore struct {
+    db    *sqlx.DB
+    store map[string]PostgresPermissionCache
+    mu    sync.RWMutex // Mutex for thread-safe map access
+}
+
+func NewPostgresPermissionStore(db *sqlx.DB) *PostgresPermissionStore {
+    return &PostgresPermissionStore{
+        db:    db,
+        store: make(map[string]PostgresPermissionCache),
+    }
+}
+
+func (s *PostgresPermissionStore) HasPermission(accountId string, permissions []string) bool {
+    // Read lock (multiple readers allowed)
+    s.mu.RLock()
+    cache, isInCache := s.store[accountId]
+    s.mu.RUnlock()
+
+    // If cache is expired or not found, refresh it
+    if !isInCache || time.Now().After(cache.ValidUntil) {
+        s.mu.Lock() // Exclusive write lock
+        if !isInCache || time.Now().After(cache.ValidUntil) { // Re-check to avoid duplicate DB calls
+            cache = PostgresPermissionCache{
+                Permissions: []string{},
+                ValidUntil:  time.Now().Add(15 * time.Minute),
+            }
+
+            query := `
+                SELECT system.role_permission.value 
+                FROM system.account_role 
+                INNER JOIN system.role ON account_role.role_id = role.id
+                INNER JOIN system.role_permission ON role.id = role_permission.role_id
+                WHERE account_role.account_id = $1
+            `
+
+            err := s.db.Select(&cache.Permissions, query, accountId)
+            if err != nil {
+                s.mu.Unlock()
+                fmt.Printf("Error fetching permissions for account %s: %v\n", accountId, err)
+                return false
+            }
+
+            s.store[accountId] = cache // Update cache safely
+        }
+        s.mu.Unlock()
+    }
+
+    // Optimized permission checking using a map
+    permMap := make(map[string]struct{}, len(cache.Permissions))
+    for _, perm := range cache.Permissions {
+        permMap[perm] = struct{}{}
+    }
+
+    for _, permission := range permissions {
+        if _, exists := permMap[permission]; exists {
+            return true
+        }
+    }
+
+    return false
+}
+
+func (s *PostgresPermissionStore) Invalidate() {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.store = make(map[string]PostgresPermissionCache) // Reset cache safely
 }
