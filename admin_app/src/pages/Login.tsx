ade845f1e712a63c58f4ead7e340de061fedee64
Ryan Ali <ryanali456@gmail.com>
2025-03-20 13:12:13 +0800

    update login page

--- admin_app/src/pages/Login.tsx
+++ admin_app/src/pages/Login.tsx
@@ -1,9 +1,13 @@
 import { loginRequest } from "@definitions/configs/msal/msal.config";
 import { useMsal } from "@azure/msal-react";
-
+import { Button } from "flowbite-react";
+import libraryIcon from "@assets/images/library-icon.svg"
+import { FormEvent } from "react";
+import { TiVendorMicrosoft } from "react-icons/ti"
 const Login = () => {
   const { instance: msalClient } = useMsal();
-  const handleLogin = async () => {
+  const handleLogin = async (event: FormEvent<HTMLFormElement> ) => {
+    event.preventDefault()
     try {
       await msalClient.loginRedirect(loginRequest);
     } catch (err) {
@@ -11,59 +15,32 @@ const Login = () => {
     }
   };
   return (
-    <div className="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200 min-h-screen">
-      <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40 flex">
-        <div className="m-auto md:w-8/12 lg:w-3/12 xl:w-5/12">
-          <div
-            className="rounded-xl bg-white shadow-xl"
-            style={{ height: "460px" }}
-          >
-            <div className="p-6 sm:p-16">
-              <div className="space-y-4">
-                <img
-                  src="https://tailus.io/sources/blocks/social/preview/images/icon.svg"
-                  loading="lazy"
-                  className="w-10"
-                  alt="tailus logo"
-                />
-                <h2 className="text-2xl text-cyan-900 font-bold">
-                  Sign in to Manage
-                  <br /> the Library
-                </h2>
-                <p>
-                  Securely sign in to manage and maintain the school's library
+  <section className="bg-gray-50 dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" className="flex flex-col items-center gap-1  mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          
+      </a>
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <img className="w-14 h-14 mr-2" src={libraryIcon} alt="logo"></img>
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              STI College Muñoz-EDSA Library
+              </h1>
+              <p className="text-gray-900 dark:text-white">
+                  Securely sign in to manage and maintain the library
                   resource.
-                </p>
-              </div>
-              <div className="mt-8 grid space-y-4">
-                <button
-                  className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
-     hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
-                  onClick={handleLogin}
-                >
-                  <div className="relative flex items-center space-x-4 justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 48 48"
-                      width="30px"
-                      height="30px"
-                    >
-                      <path
-                        fill="#e64a19"
-                        d="M7 12L29 4 41 7 41 41 29 44 7 36 29 39 29 10 15 13 15 33 7 36z"
-                      />
-                    </svg>
-                    <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-                      Library Staff Login
-                    </span>
-                  </div>
-                </button>
-              </div>
-            </div>
+              </p>
+              <form onSubmit={handleLogin} className="space-y-4 md:space-y-6" action="#">
+                <Button color="primary" className="w-full" type="submit">
+                
+                <TiVendorMicrosoft className="text-xl md:text-2xl mr-2"/>
+                  
+                  <span className="text-base md:text-lg">Sign In</span></Button>
+              </form>
           </div>
-        </div>
       </div>
-    </div>
+  </div>
+</section>
   );
 };
 
